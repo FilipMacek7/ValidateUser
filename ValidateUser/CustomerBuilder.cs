89e88b099136378050e59704926379a08f31464c
maceklu161 <maceklu161@DST20703>
2018-11-22 16:06:29 +0100

    Přidejte soubory projektu.

---
+++ ValidateUser/CustomerBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidateUser
+{
+    public class CustomerBuilder
+    {
+        public string Surname { get; set; }
+        public string Forename { get; set; }
+        public int BirthYear { get; set; }
+
+        public CustomerBuilder WithSurName(string surname)
+        {
+            this.Surname = surname;
+            return this;
+        }
+
+        public CustomerBuilder WithForeName(string forename)
+        {
+            this.Forename = forename;
+            return this;
+        }
+        public CustomerBuilder WithBirthYear(int birthyear)
+        {
+            this.BirthYear = birthyear;
+            return this;
+        }
+    }
+}
---
+++ ValidateUser/CustomerValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace ValidateUser
+{
+    public class CustomerValidator : AbstractValidator<CustomerBuilder>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(customer => customer.Surname)
+                .NotNull()
+                .NotEqual("foo");
+
+            RuleFor(customer => customer.Forename)
+                 .NotNull()
+                 .NotEqual("foo");
+            RuleFor(customer => customer.BirthYear)
+                 .NotNull()
+                 .LessThan(2018);
+        }
+
+    }
+}
